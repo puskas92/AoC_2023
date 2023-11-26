b619e338bb204ec81f2e85a42a30b7ec4c1996c1
Puskas David <puskas.david92@gmail.com>
2023-11-26 13:49:53 +0100

    Add project files.

---
+++ AoC_2023/Program.cs
@@ -0,0 +1,12 @@
+global using Xunit;
+using AoC_2023;
+using System.Diagnostics;
+
+Console.WriteLine("AoC 2023");
+var sw = new Stopwatch();
+sw.Start();
+//Day01.Day01_Main();
+sw.Stop();
+
+Console.WriteLine($"Code run under {sw.ElapsedMilliseconds}ms");
+Console.ReadLine();
