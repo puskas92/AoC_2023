5faecc26e32fa2275171e72f9ea7b0a10873b556
Puskas David <puskas.david92@gmail.com>
2023-12-01 07:29:29 +0100

    Day01 solution

--- AoC_2023/Day01/Day01.cs
+++ AoC_2023/Day01/Day01.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.XPath;
@@ -32,7 +33,7 @@ namespace AoC_2023
 
             foreach (string line in rawinput.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Select(s => s.Trim()))
             {
-                //result.Add(line);
+               result.Add(line);
             }
 
             return result;
@@ -41,28 +42,31 @@ namespace AoC_2023
 
         public static int Day01_Part1(Day01_Input input)
         {
-
-            return 0;
+            char[] nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
+            return input.Sum(f =>
+                       int.Parse(string.Concat(f[f.IndexOfAny(nums)], f[f.LastIndexOfAny(nums)])));
         }
 
         public static int Day01_Part2(Day01_Input input)
         {
-            return 0;
+            var modifiedInput = new List<string>();
+            modifiedInput = input.Select(f => f.Replace("one", "o1e").Replace("two", "t2o").Replace("three", "o3").Replace("four", "4").Replace("five", "5e").Replace("six", "6").Replace("seven", "7n").Replace("eight", "e8t").Replace("nine", "n9e")).ToList();
+            char[] nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+            return modifiedInput.Sum(f =>
+                       int.Parse(string.Concat(f[f.IndexOfAny(nums)], f[f.LastIndexOfAny(nums)])));
         }
-
-
     }
     public class Day01_Test
     {
         [Theory]
-        [InlineData("ABC", 0)]
+        [InlineData("1abc2\r\npqr3stu8vwx\r\na1b2c3d4e5f\r\ntreb7uchet", 142)]
         public static void Day01Part1Test(string rawinput, int expectedValue)
         {
             Assert.Equal(expectedValue, Day01.Day01_Part1(Day01.Day01_ReadInput(rawinput)));
         }
 
         [Theory]
-        [InlineData("ABC", 0)]
+        [InlineData("two1nine\r\neightwothree\r\nabcone2threexyz\r\nxtwone3four\r\n4nineeightseven2\r\nzoneight234\r\n7pqrstsixteen", 281)]
         public static void Day01Part2Test(string rawinput, int expectedValue)
         {
             Assert.Equal(expectedValue, Day01.Day01_Part2(Day01.Day01_ReadInput(rawinput)));
---
+++ AoC_2023/Day02/Day02.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using Xunit;
+
+namespace AoC_2023
+{
+    public static class Day02
+    {
+        public class Day02_Input : List<string> //Define input type
+        {
+        }
+        public static void Day02_Main()
+        {
+            var input = Day02_ReadInput();
+            Console.WriteLine($"Day02 Part1: {Day02_Part1(input)}");
+            Console.WriteLine($"Day02 Part2: {Day02_Part2(input)}");
+        }
+
+        public static Day02_Input Day02_ReadInput(string rawinput = "")
+        {
+            if (rawinput == "")
+            {
+                rawinput = new StreamReader("Day02\\Day02_input.txt").ReadToEnd();
+            }
+
+            var result = new Day02_Input();
+
+            foreach (string line in rawinput.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Select(s => s.Trim()))
+            {
+                //result.Add(line);
+            }
+
+            return result;
+        }
+
+
+        public static int Day02_Part1(Day02_Input input)
+        {
+
+            return 0;
+        }
+
+        public static int Day02_Part2(Day02_Input input)
+        {
+            return 0;
+        }
+
+
+    }
+    public class Day02_Test
+    {
+        [Theory]
+        [InlineData("ABC", 0)]
+        public static void Day02Part1Test(string rawinput, int expectedValue)
+        {
+            Assert.Equal(expectedValue, Day02.Day02_Part1(Day02.Day02_ReadInput(rawinput)));
+        }
+
+        [Theory]
+        [InlineData("ABC", 0)]
+        public static void Day02Part2Test(string rawinput, int expectedValue)
+        {
+            Assert.Equal(expectedValue, Day02.Day02_Part2(Day02.Day02_ReadInput(rawinput)));
+        }
+    }
+}
