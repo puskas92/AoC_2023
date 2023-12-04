91c7dce1e9ff7bcde5c8bcba09887ae4952c9459
Puskas David <puskas.david92@gmail.com>
2023-12-04 20:54:01 +0100

    Day04 Solution

--- AoC_2023/Day04/Day04.cs
+++ AoC_2023/Day04/Day04.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.XPath;
@@ -11,9 +12,28 @@ namespace AoC_2023
 {
     public static class Day04
     {
-        public class Day04_Input : List<string> //Define input type
+        public class Day04_Input : Dictionary<int, Day04_Card> //Define input type
         {
         }
+
+        public class Day04_Card
+        {
+            public int numberOfMatch;
+            public int points;
+            public int id;
+            public List<int> winningNumbers;
+            public List<int> numbersYouHave;
+
+            public Day04_Card(int id, List<int> winningNumbers, List<int> numbersYouHave)
+            {
+                this.id = id;
+                this.winningNumbers = winningNumbers;
+                this.numbersYouHave = numbersYouHave;
+
+                numberOfMatch = numbersYouHave.Intersect(winningNumbers).Count();
+                points = (numberOfMatch == 0) ? 0 : (int)Math.Pow(2, numberOfMatch - 1);
+            }
+        };
         public static void Day04_Main()
         {
             var input = Day04_ReadInput();
@@ -32,7 +52,11 @@ namespace AoC_2023
 
             foreach (string line in rawinput.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Select(s => s.Trim()))
             {
-                //result.Add(line);
+                var id = int.Parse(line.Split(':')[0].Split(' ').Last());
+                var winningNumbers = line.Split(':')[1].Split('|')[0].Trim().Split(' ').Where(f=> f!="").Select(f => int.Parse(f)).ToList();
+                var numbersYouHave = line.Split('|')[1].Trim().Split(' ').Where(f => f != "").Select(f => int.Parse(f)).ToList();
+                var card = new Day04_Card(id,winningNumbers,numbersYouHave);
+                result.Add(id, card);
             }
 
             return result;
@@ -41,13 +65,23 @@ namespace AoC_2023
 
         public static int Day04_Part1(Day04_Input input)
         {
-
-            return 0;
+            return input.Sum(f => f.Value.points);
         }
 
         public static int Day04_Part2(Day04_Input input)
         {
-            return 0;
+            var numberOfCards = new List<int>();
+            foreach (var card in input) numberOfCards.Add(1);
+            for (int i = 0; i<=input.Count-1; i++)
+            {
+                for(int j = 1; j <= input[i+1].numberOfMatch; j++)
+                {
+                    if (numberOfCards.Count < (i + j)) continue;
+                    numberOfCards[i + j] += numberOfCards[i];
+                }
+            }
+
+            return numberOfCards.Sum();
         }
 
 
@@ -55,14 +89,14 @@ namespace AoC_2023
     public class Day04_Test
     {
         [Theory]
-        [InlineData("ABC", 0)]
+        [InlineData("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\r\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\r\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\r\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\r\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\r\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 13)]
         public static void Day04Part1Test(string rawinput, int expectedValue)
         {
             Assert.Equal(expectedValue, Day04.Day04_Part1(Day04.Day04_ReadInput(rawinput)));
         }
 
         [Theory]
-        [InlineData("ABC", 0)]
+        [InlineData("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\r\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\r\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\r\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\r\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\r\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 30)]
         public static void Day04Part2Test(string rawinput, int expectedValue)
         {
             Assert.Equal(expectedValue, Day04.Day04_Part2(Day04.Day04_ReadInput(rawinput)));
---
+++ AoC_2023/Day05/Day05.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using Xunit;
+
+namespace AoC_2023
+{
+    public static class Day05
+    {
+        public class Day05_Input : List<string> //Define input type
+        {
+        }
+        public static void Day05_Main()
+        {
+            var input = Day05_ReadInput();
+            Console.WriteLine($"Day05 Part1: {Day05_Part1(input)}");
+            Console.WriteLine($"Day05 Part2: {Day05_Part2(input)}");
+        }
+
+        public static Day05_Input Day05_ReadInput(string rawinput = "")
+        {
+            if (rawinput == "")
+            {
+                rawinput = new StreamReader("Day05\\Day05_input.txt").ReadToEnd();
+            }
+
+            var result = new Day05_Input();
+
+            foreach (string line in rawinput.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).Select(s => s.Trim()))
+            {
+                //result.Add(line);
+            }
+
+            return result;
+        }
+
+
+        public static int Day05_Part1(Day05_Input input)
+        {
+
+            return 0;
+        }
+
+        public static int Day05_Part2(Day05_Input input)
+        {
+            return 0;
+        }
+
+
+    }
+    public class Day05_Test
+    {
+        [Theory]
+        [InlineData("ABC", 0)]
+        public static void Day05Part1Test(string rawinput, int expectedValue)
+        {
+            Assert.Equal(expectedValue, Day05.Day05_Part1(Day05.Day05_ReadInput(rawinput)));
+        }
+
+        [Theory]
+        [InlineData("ABC", 0)]
+        public static void Day05Part2Test(string rawinput, int expectedValue)
+        {
+            Assert.Equal(expectedValue, Day05.Day05_Part2(Day05.Day05_ReadInput(rawinput)));
+        }
+    }
+}
