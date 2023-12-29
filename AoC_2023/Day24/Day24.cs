d043b90b99b07767eb6f1088a4c7a2f6fabfd9c5
Puskas David <puskas.david92@gmail.com>
2023-12-29 12:01:18 +0100

    try to solve Day24 with convergence, bit it diverge

--- AoC_2023/Day24/Day24.cs
+++ AoC_2023/Day24/Day24.cs
@@ -54,20 +54,6 @@ namespace AoC_2023
                     var test1 = input[i];
                     var test2 = input[j];
 
-                    //double t = -1 * (double)(test1.p.X - test2.p.X) / (double)(test1.v.X - test2.v.X);
-
-                    //var x = test1.p.X + t * test1.v.X;
-
-                    //var y1 = test1.p.Y + t * test1.v.Y;
-                    //var y2 = test2.p.Y + t * test2.v.Y;
-                    //if (y1 != y2) continue;
-
-                    ////var z1 = test1.p.Z + t * test1.v.Z;
-                    ////var z2 = test2.p.Z + t * test2.v.Z;
-                    ////if (z1 != z2) continue;
-
-                    //if ((x >= LimitLow && x <= LimitHigh) && (y1 >= LimitLow && y1 <= LimitHigh)) collosionCount += 1;
-
                     var a1 = test1.v.Y;
                     var b1 = -test1.v.X;
                     var c1 = test1.p.Y * test1.v.X - test1.p.X * test1.v.Y;
@@ -97,9 +83,103 @@ namespace AoC_2023
             return collosionCount;
         }
 
-        public static int Day24_Part2(Day24_Input input)
+        public static bool Day24_IsHailstoneCollide(Day24_Hailstone test1, Day24_Hailstone test2)
         {
-            return 0;
+            if ((test1.v.X - test2.v.X) == 0) return false;
+
+            double t = -1 * (double)(test1.p.X - test2.p.X) / (double)(test1.v.X - test2.v.X);
+
+            var x = test1.p.X + t * test1.v.X;
+
+            var y1 = test1.p.Y + t * test1.v.Y;
+            var y2 = test2.p.Y + t * test2.v.Y;
+            if (y1 != y2) return false;
+
+            var z1 = test1.p.Z + t * test1.v.Z;
+            var z2 = test2.p.Z + t * test2.v.Z;
+            if (z1 != z2) return false;
+
+            return true;
+        }
+        public static long Day24_Part2(Day24_Input input)
+        {
+            long px_avr = 0, py_avr =0, pz_avr = 0;
+            long vx_avr = 0, vy_avr = 0, vz_avr = 0;
+            var n = input.Count();
+            List<int> time = new List<int>();
+            foreach (var hailstone in input)
+            {
+                px_avr += hailstone.p.X;
+                py_avr += hailstone.p.Y;
+                pz_avr += hailstone.p.Z;
+                vx_avr += hailstone.v.X;
+                vy_avr += hailstone.v.Y;
+                vz_avr += hailstone.v.Z;
+                time.Add(0);
+            }
+            px_avr = px_avr / n;
+            py_avr = py_avr / n;
+            pz_avr = pz_avr / n;
+            vx_avr = vx_avr / n;
+            vy_avr = vy_avr / n;
+            vz_avr = vz_avr / n;
+
+            var px = px_avr;
+            var py = py_avr;
+            var pz = pz_avr;
+            var vx = vx_avr;
+            var vy = vy_avr;
+            var vz = vz_avr;
+            var rock = new Day24_Hailstone((px, py, pz), (vx, vy, vz));
+
+            while(!input.All(f=> Day24_IsHailstoneCollide(f, rock)))
+            {
+                double vx_delta = 0;
+                double vy_delta = 0;
+                double vz_delta = 0;
+                for (var i = 0; i< input.Count; i++)
+                {
+                    var testHail = input[i];
+                    double tx = -1 * (px - testHail.p.X) / (double)(vx - testHail.v.X);
+                    double ty = -1 * (py - testHail.p.Y) / (double)(vy - testHail.v.Y);
+                    double tz = -1 * (pz - testHail.p.Z) / (double)(vz - testHail.v.Z);
+                    var c = 0;
+                    double t = 0;
+                    if (double.IsFinite(tx))
+                    {
+                        t += tx;
+                        c++;
+                    }
+                    if (double.IsFinite(ty))
+                    {
+                        t += ty;
+                        c++;
+                    }
+                    if (double.IsFinite(tz))
+                    {
+                        t += tz;
+                        c++;
+                    }
+                    t = (c == 0) ? time[i] : ((t / c) + time[i] /2) ;
+                    if (t <= 0) t = 1;
+                    time[i] = (int)Math.Round(t);
+
+                    vx = (int)Math.Round((((testHail.p.X - px) / t + testHail.v.X) + vx)/2);
+                    vy = (int)Math.Round((((testHail.p.Y - py) / t + testHail.v.Y) + vy)/2);
+                    vz = (int)Math.Round((((testHail.p.Z - pz) / t + testHail.v.Z) + vz)/2);
+
+                    vx_delta += t * (testHail.v.X - vx);
+                    vy_delta += t * (testHail.v.Y - vy);
+                    vz_delta += t  * (testHail.v.Z - vz);
+                }
+                px = px_avr + (int)Math.Round(vx_delta / n);
+                py = py_avr + (int)Math.Round(vy_delta / n);
+                pz = pz_avr + (int)Math.Round(vz_delta / n);
+            }
+
+
+            return (px_avr + py_avr + pz_avr);
+            //868366741881308 too high
         }
 
 
