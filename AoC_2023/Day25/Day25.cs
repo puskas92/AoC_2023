61914a4e6f3fd2d17aef260a5fd25a1ae08a0386
Puskas David <puskas.david92@gmail.com>
2023-12-30 14:29:00 +0100

    Day25 Solution

--- AoC_2023/Day25/Day25.cs
+++ AoC_2023/Day25/Day25.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
@@ -50,55 +52,152 @@ namespace AoC_2023
 
         public static int Day25_Part1(Day25_Input input)
         {
-            var ToCheckQueue = new Queue<(string currentPos, List<string> visited, int length)>();
-            ToCheckQueue.Enqueue((input.First().Key, new List<string>(), 0));
-            var resultList = new List<(List<string> visited, int length)>();
-            while (ToCheckQueue.Count > 0)
-            {
-                var ToCheck = ToCheckQueue.Dequeue();
-                if (input[ToCheck.currentPos].Count == 0)
-                {
-                    resultList.Add((new List<string>(ToCheck.visited), ToCheck.length));
-                    continue;
-                }
-                foreach (var dest in input[ToCheck.currentPos])
-                {
-                    if (ToCheck.visited.Contains(dest))
-                    {
-                        resultList.Add((new List<string>(ToCheck.visited), ToCheck.length));
-                        continue;
-                    }
-                    var newvisitied = new List<string>(ToCheck.visited);
-                    newvisitied.Add(dest);
-                    ToCheckQueue.Enqueue((dest, newvisitied, ToCheck.length + 1));
-                }
-            }
-
-            var maxLength = resultList.Max(f => f.length);
-            var numberOfUsage = new Dictionary<string, int>();
-            //foreach (var line in input)
+            //var ToCheckQueue = new Queue<(string currentPos, List<string> visited, int length)>();
+            //ToCheckQueue.Enqueue((input.First().Key, new List<string>(), 0));
+            //var resultList = new List<(List<string> visited, int length)>();
+            //while (ToCheckQueue.Count > 0)
             //{
-            //    numberOfUsage.Add(line.Key, 0);
+            //    var ToCheck = ToCheckQueue.Dequeue();
+            //    if (input[ToCheck.currentPos].Count == 0)
+            //    {
+            //        resultList.Add((new List<string>(ToCheck.visited), ToCheck.length));
+            //        continue;
+            //    }
+            //    foreach (var dest in input[ToCheck.currentPos])
+            //    {
+            //        if (ToCheck.visited.Contains(dest))
+            //        {
+            //            resultList.Add((new List<string>(ToCheck.visited), ToCheck.length));
+            //            continue;
+            //        }
+            //        var newvisitied = new List<string>(ToCheck.visited);
+            //        newvisitied.Add(dest);
+            //        ToCheckQueue.Enqueue((dest, newvisitied, ToCheck.length + 1));
+            //    }
             //}
-            foreach (var resultline in resultList.Where(f=> f.length == maxLength))
+
+            //var maxLength = resultList.Max(f => f.length);
+            //var numberOfUsage = new Dictionary<string, int>();
+            ////foreach (var line in input)
+            ////{
+            ////    numberOfUsage.Add(line.Key, 0);
+            ////}
+            //foreach (var resultline in resultList.Where(f=> f.length == maxLength))
+            //{
+            //    //foreach(var part in resultline.visited)
+            //    //{
+            //    //    numberOfUsage[part]++;
+            //    //}
+            //    for (var i = 0; i < resultline.visited.Count - 1; i++)
+            //    {
+            //        var key = resultline.visited[i] + ',' + resultline.visited[i+1];
+            //        if(!numberOfUsage.ContainsKey(key)) numberOfUsage.Add(key,0);
+            //        numberOfUsage[key]++;
+            //    }
+            //}
+            //var OrderedNumberOfUsage = numberOfUsage.OrderBy(f=> f.Value).ToList();
+            //return 0;
+
+            Day25_Input contractedGraph;
+            do
             {
-                //foreach(var part in resultline.visited)
-                //{
-                //    numberOfUsage[part]++;
-                //}
-                for (var i = 0; i < resultline.visited.Count - 1; i++)
+                contractedGraph = new Day25_Input();
+                foreach (var node in input)
                 {
-                    var key = resultline.visited[i] + ',' + resultline.visited[i+1];
-                    if(!numberOfUsage.ContainsKey(key)) numberOfUsage.Add(key,0);
-                    numberOfUsage[key]++;
+                    contractedGraph.Add(node.Key, new List<string>(node.Value));
                 }
-            }
-            var OrderedNumberOfUsage = numberOfUsage.OrderBy(f=> f.Value).ToList();
-            return 0;
+
+                contractedGraph = fastMinCut(contractedGraph);
+            } while (contractedGraph.First().Value.Count > 3);
+
+            var first = contractedGraph.First().Key.Split(',');
+            var second = contractedGraph.Last().Key.Split(',');
+
+            return first.Count() * second.Count();
         }
 
+        private static Day25_Input fastMinCut(Day25_Input graph)
+        {
+            if(graph.Count <= 6)
+            {
+                return contract(graph, 2);
+            }
+            else
+            {
+                var t = (1 + (Math.Abs(graph.Count) / Math.Sqrt(2)));
+
+                var graph1 = new Day25_Input();
+                foreach (var node in graph)
+                {
+                    graph1.Add(node.Key, new List<string>(node.Value));
+                }
+                graph1 =  contract(graph1, t);
+
+                var graph2 = new Day25_Input();
+                foreach (var node in graph)
+                {
+                    graph2.Add(node.Key, new List<string>(node.Value));
+                }
+                graph2 = contract(graph2, t);
+
+                graph1 = fastMinCut(graph1);
+                graph2 = fastMinCut(graph2);
+
+                return (graph1.First().Value.Count <= graph2.First().Value.Count) ? graph1 : graph2;
+            }
+        }
+        private static Day25_Input contract(Day25_Input graph, double untilSize)
+        {
 
 
+            while (graph.Count > untilSize)
+            {
+                var listOfEdges = new List<(string, string)>(); //will contain all edges twice, with both direction, but this should not be a problem
+                foreach (var node in graph)
+                {
+                    foreach (var con in node.Value)
+                    {
+                        listOfEdges.Add((node.Key, con));
+                    }
+                }
+
+                //randomly select an edge
+                var rnd = Random.Shared.Next(listOfEdges.Count);
+                var removeEdge = listOfEdges[rnd];
+
+                //create new contracted edge
+                var newEdge = removeEdge.Item1 + ',' + removeEdge.Item2;
+                graph.Add(newEdge, new List<string>());
+                foreach (var node in graph[removeEdge.Item1])
+                {
+                    graph[newEdge].Add(node);
+                }
+                foreach (var node in graph[removeEdge.Item2])
+                {
+                    graph[newEdge].Add(node);
+                }
+                //delete old edge
+                var result = graph.Remove(removeEdge.Item1);
+                Debug.Assert(result);
+                result = graph.Remove(removeEdge.Item2);
+                Debug.Assert(result);
+
+                //update all the other nodes with newedge
+                foreach (var node in graph)
+                {
+                    for (var i = 0; i < node.Value.Count; i++)
+                    {
+                        if (node.Value[i] == removeEdge.Item1) node.Value[i] = newEdge;
+                        if (node.Value[i] == removeEdge.Item2) node.Value[i] = newEdge;
+                    }
+                    //remove self edge
+                    node.Value.RemoveAll(f => f == node.Key);
+                }
+
+            }
+
+            return graph;
+        }
     }
     public class Day25_Test
     {
--- AoC_2023/Program.cs
+++ AoC_2023/Program.cs
@@ -5,30 +5,30 @@ using System.Diagnostics;
 Console.WriteLine("AoC 2023");
 var sw = new Stopwatch();
 sw.Start();
-//Day01.Day01_Main();
-//Day02.Day02_Main();
-//Day03.Day03_Main();
-//Day04.Day04_Main();
-//Day05.Day05_Main();
-//Day06.Day06_Main();
-//Day07.Day07_Main();
-//Day08.Day08_Main();
-//Day09.Day09_Main();
-//Day10.Day10_Main(); // could be faster
-//Day11.Day11_Main(); // could be faster
-//Day12.Day12_Main();
-//Day13.Day13_Main();
-//Day14.Day14_Main(); //could be faster
-//Day15.Day15_Main();
-//Day16.Day16_Main(); //could be faster
-//Day17.Day17_Main();
-//Day18.Day18_Main();
-//Day19.Day19_Main();
-//Day20.Day20_Main();
-//Day21.Day21_Main();
-//Day22.Day22_Main(); //could be faster, but it works in reasonable time (~80s)
-//Day23.Day23_Main(); //Part 2 could be faster
-//Day24.Day24_Main(); //Part 2 solved in symbolic math program
+Day01.Day01_Main();
+Day02.Day02_Main();
+Day03.Day03_Main();
+Day04.Day04_Main();
+Day05.Day05_Main();
+Day06.Day06_Main();
+Day07.Day07_Main();
+Day08.Day08_Main();
+Day09.Day09_Main();
+Day10.Day10_Main(); // could be faster
+Day11.Day11_Main(); // could be faster
+Day12.Day12_Main();
+Day13.Day13_Main();
+Day14.Day14_Main(); //could be faster
+Day15.Day15_Main();
+Day16.Day16_Main(); //could be faster
+Day17.Day17_Main();
+Day18.Day18_Main();
+Day19.Day19_Main();
+Day20.Day20_Main();
+Day21.Day21_Main();
+Day22.Day22_Main(); //could be faster, but it works in reasonable time (~80s)
+Day23.Day23_Main(); //Part 2 could be faster
+Day24.Day24_Main(); //Part 2 solved in symbolic math program
 Day25.Day25_Main();
 sw.Stop();
 
