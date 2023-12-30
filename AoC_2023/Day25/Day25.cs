1d9115a374c9a48abe1f42aae16e78c99c1e0d4f
Puskas David <puskas.david92@gmail.com>
2023-12-30 17:20:13 +0100

    day25 remove uneccesary commented out parts

--- AoC_2023/Day25/Day25.cs
+++ AoC_2023/Day25/Day25.cs
@@ -52,52 +52,6 @@ namespace AoC_2023
 
         public static int Day25_Part1(Day25_Input input)
         {
-            //var ToCheckQueue = new Queue<(string currentPos, List<string> visited, int length)>();
-            //ToCheckQueue.Enqueue((input.First().Key, new List<string>(), 0));
-            //var resultList = new List<(List<string> visited, int length)>();
-            //while (ToCheckQueue.Count > 0)
-            //{
-            //    var ToCheck = ToCheckQueue.Dequeue();
-            //    if (input[ToCheck.currentPos].Count == 0)
-            //    {
-            //        resultList.Add((new List<string>(ToCheck.visited), ToCheck.length));
-            //        continue;
-            //    }
-            //    foreach (var dest in input[ToCheck.currentPos])
-            //    {
-            //        if (ToCheck.visited.Contains(dest))
-            //        {
-            //            resultList.Add((new List<string>(ToCheck.visited), ToCheck.length));
-            //            continue;
-            //        }
-            //        var newvisitied = new List<string>(ToCheck.visited);
-            //        newvisitied.Add(dest);
-            //        ToCheckQueue.Enqueue((dest, newvisitied, ToCheck.length + 1));
-            //    }
-            //}
-
-            //var maxLength = resultList.Max(f => f.length);
-            //var numberOfUsage = new Dictionary<string, int>();
-            ////foreach (var line in input)
-            ////{
-            ////    numberOfUsage.Add(line.Key, 0);
-            ////}
-            //foreach (var resultline in resultList.Where(f=> f.length == maxLength))
-            //{
-            //    //foreach(var part in resultline.visited)
-            //    //{
-            //    //    numberOfUsage[part]++;
-            //    //}
-            //    for (var i = 0; i < resultline.visited.Count - 1; i++)
-            //    {
-            //        var key = resultline.visited[i] + ',' + resultline.visited[i+1];
-            //        if(!numberOfUsage.ContainsKey(key)) numberOfUsage.Add(key,0);
-            //        numberOfUsage[key]++;
-            //    }
-            //}
-            //var OrderedNumberOfUsage = numberOfUsage.OrderBy(f=> f.Value).ToList();
-            //return 0;
-
             Day25_Input contractedGraph;
             do
             {
